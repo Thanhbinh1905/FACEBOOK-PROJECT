4457c9b5e2dd2a01438335074645addee94d2b26
Thanhbinh1905 <thanhbinh2003195@gmail.com>
2022-07-22 04:08:42 +0700

    add html file

--- contact/Sign_In/signin.js
+++ contact/Sign_In/signin.js
@@ -4,7 +4,7 @@ function validate(){
   var password = document.getElementById("password").value;
   if ( username == "binhquangkhanhit12" && password == "bo3acquy"){
   alert ("Login successfully");
-  window.location.href = "../../home/home-index.html"; // Redirecting to other page.
+  window.location.href = "home/home-index.html"; // Redirecting to other page.
   }
   else{
   alert ("incorect password or name");
